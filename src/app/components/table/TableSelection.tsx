bc2203373151275aed3870399b67b2ac69d28aa7
Nonito <nonitojunas13@gmail.com>
2024-07-25 15:57:16 +0800

    first commit

---
+++ src/app/components/table/TableSelection.tsx
@@ -0,0 +1,62 @@
+'use client'
+import cx from 'clsx';
+import { useState } from 'react';
+import { Table, Checkbox, ScrollArea, Group, Avatar, Text, rem } from '@mantine/core';
+import classes from './TableSelection.module.css';
+
+
+
+export function TableSelection({data} : {data:any []}) {
+  const [selection, setSelection] = useState(['1']);
+  const toggleRow = (id: string) =>
+    setSelection((current) =>
+      current.includes(id) ? current.filter((item) => item !== id) : [...current, id]
+    );
+  const toggleAll = () =>
+    setSelection((current) => (current.length === data.length ? [] : data.map((item) => item.id)));
+
+  const rows = data.map((item) => {
+    const selected = selection.includes(item.id);
+    return (
+      <Table.Tr key={item.id} className={cx({ [classes.rowSelected]: selected })}>
+        <Table.Td>
+          <Checkbox checked={selection.includes(item.id)} onChange={() => toggleRow(item.id)} />
+        </Table.Td>
+        <Table.Td>
+          <Group gap="sm">
+            <Avatar size={26} src={item.avatar} radius={26} />
+            <Text size="sm" fw={500}>
+              {item.firstname}
+            </Text>
+          </Group>
+        </Table.Td>
+        
+        <Table.Td>{item.lastname}</Table.Td>
+        <Table.Td>{item.email}</Table.Td>
+      </Table.Tr>
+    );
+  });
+
+  return (
+    <ScrollArea>
+      <Table miw={800} verticalSpacing="sm">
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th style={{ width: rem(40) }}>
+              <Checkbox
+                onChange={toggleAll}
+                checked={selection.length === data.length}
+                indeterminate={selection.length > 0 && selection.length !== data.length}
+              />
+            </Table.Th>
+            <Table.Th>First Name</Table.Th>
+            
+            <Table.Th>Last Name</Table.Th>
+            <Table.Th>Email</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>{rows}</Table.Tbody>
+      </Table>
+    </ScrollArea>
+  );
+}
